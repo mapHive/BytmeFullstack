9a4866ad4dc463062f66a31323e66586a09c3ecf
mapHive <assembly@kiranbrar.dev>
2023-05-13 16:26:58 +0800

    Changes

--- src/main/java/org/generation/byteme/component/FileUploadUtil.java
+++ src/main/java/org/generation/byteme/component/FileUploadUtil.java
@@ -7,12 +7,12 @@ import java.nio.file.*;
 
 public class FileUploadUtil {
 
-    public static void saveFile(String uploadDir1, String fileName, MultipartFile multipartFile) throws IOException
+    public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException
     {
-        Path uploadPath1 = Paths.get(uploadDir1);
+        Path uploadPath = Paths.get(uploadDir);
         try (InputStream inputStream = multipartFile.getInputStream()) {
 
-            Path filePath = uploadPath1.resolve(fileName);
+            Path filePath = uploadPath.resolve(fileName);
             Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException ioe) {
             throw new IOException("Could not save image file: " + fileName, ioe);
