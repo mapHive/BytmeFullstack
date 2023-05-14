aaa027faffe553cfe75acc2d03002c5b11511240
mapHive <assembly@kiranbrar.dev>
2023-05-14 14:28:00 +0800

    Changes

--- src/main/resources/static/js/featuredItems.js
+++ src/main/resources/static/js/featuredItems.js
@@ -1,42 +1,81 @@
 
-const featuredItems = [];
+// Development APIs
+const addAPI= 'http://localhost:8080/product/add';
+const displayAPI = 'http://localhost:8080/product/all';
+let featuredItems = [];
 
-function displayProduct() {
+function displayProduct()
+{
+//fetch data from database using the REST API endpoint from Spring Boot
+       fetch(displayAPI)
+           .then((resp) => resp.json())
+           .then(function(data) {
+               console.log("2. receive data")
+               console.log(data);
+               data.forEach(function (product) {
 
-  let display = "";
+                   const productObj = {
+                       productId: product.productId,
+                       productName: product.productName,
+                       productPrice: product.productPrice,
+                       productQuantity: product.productQuantity,
+                       productCategory: product.productCategory,
+                       productDescription: product.productDescription,
+                       productOptions: product.productOptions,
+                       productImages: product.productImages
+                  };
 
-  for (let i = 0; i < featuredItems.length; i++) {
+                  // This array consists of 12 objects
+                   featuredItems.push(productObj);
+             });
 
-    display += `
-        <div class="col my-5">
-        <a href="product.html">
-          <div class="card" style="width: 18rem;">
-           <img class="py-3" src=${featuredItems[i].imageURL} class="card-img-top" alt="...">
-          <div class="card-body">
-           <h5 class="card-title">${featuredItems[i].name}</h5>
-           <p class="card-text py-3">${featuredItems[i].style}</p>
-        </div>
-      </div>
-      </a>
-      </div>
-    `
-  }
+            // Display all the objects from the productController array
+             renderProductPage();
 
-  document.querySelector("#row").innerHTML = display;
+           })
+           .catch(function(error) {
+               console.log(error);
+           });
+}
 
+function renderProductPage() {
+
+    let display = "";
+
+    for (let i = 0; i < featuredItems.length; i++) {
+
+        display += `
+           <div class="col my-5">
+                <a th:href="@{product}">
+                  <div class="card" style="width: 18rem;">
+                   <img src=${featuredItems[i].productImages} class="card-img-top py-3" alt="...">
+                  <div class="card-body">
+                   <h5 class="card-title">${featuredItems[i].productName}</h5>
+                   <p class="card-text py-3">${featuredItems[i].productDescription}</p>
+                </div>
+              </div>
+              </a>
+              </div>
+            `
+    }
+
+    document.querySelector("#row").innerHTML = display;
 
 }
 
-function addProduct(n, d, i, s, p) {
 
-  const featureditems = {
-    name: n,
-    description: d,
-    imageURL: i,
-    style: s,
-    price: p
-  }
-
-  featuredItems.push(featureditems);
-
-}
+//function addProduct(productName, productPrice, productQuantity, productCategory, productDescription, productOptions, productImages) {
+//
+//  const featureditems = {
+//    productName: productName,
+//    productPrice: productPrice,
+//    productQuantity: productQuantity,
+//    productCategory: productCategory,
+//    productDescription: productDescription,
+//    productOptions: productOptions,
+//    productImages: productImages
+//  }
+//
+//  featuredItems.push(featureditems);
+//
+//}
