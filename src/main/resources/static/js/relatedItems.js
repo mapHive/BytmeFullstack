4eb2b5ac8512b3788bfa79033fd91bf23b7c2a57
mapHive <assembly@kiranbrar.dev>
2023-05-15 13:26:11 +0800

    Changes

--- src/main/resources/static/js/relatedItems.js
+++ src/main/resources/static/js/relatedItems.js
@@ -1,50 +1,69 @@
 let relatedItems = [];
 
-function displayRelatedItems()
-{
-//fetch data from database using the REST API endpoint from Spring Boot
-       fetch(displayAPI)
-           .then((resp) => resp.json())
-           .then(function(data) {
-               console.log("2. receive data")
-               console.log(data);
-               data.forEach(function (product) {
+function getProductIdFromUrl() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('productId');
+}
 
-                   const productObj = {
-                       productId: product.productId,
-                       productName: product.productName,
-                       productPrice: product.productPrice,
-                       productQuantity: product.productQuantity,
-                       productCategory: product.productCategory,
-                       productDescription: product.productDescription,
-                       productOptions: product.productOptions,
-                       productImages: product.productImages
-                  };
+let productIdFromUrl = getProductIdFromUrl();
 
-                  // This array consists of 12 objects
-                   relatedItems.push(productObj);
-             });
+function displayRelatedItems() {
+    //fetch data from database using the REST API endpoint from Spring Boot
+    fetch(displayAPI)
+        .then((resp) => resp.json())
+        .then(function (data) {
+            console.log("2. receive data");
+            console.log(data);
 
-            // Display all the objects from the productController array
-             renderRelatedItems();
+            let foundProduct = data.find(
+                (product) => product.productId == productIdFromUrl
+            );
 
-           })
-           .catch(function(error) {
-               console.log(error);
-           });
+            if (foundProduct) {
+                let productCat = foundProduct.productCategory;
+
+                let filteredData = data.filter(
+                    (product) => product.productCategory === productCat
+                );
+
+                filteredData.forEach(function (product) {
+                    const productObj = {
+                        productId: product.productId,
+                        productName: product.productName,
+                        productPrice: product.productPrice,
+                        productQuantity: product.productQuantity,
+                        productCategory: product.productCategory,
+                        productDescription: product.productDescription,
+                        productOptions: product.productOptions,
+                        productImages: product.productImages,
+                    };
+
+                    relatedItems.push(productObj);
+                });
+
+                // Display all the objects from the productController array
+                renderRelatedItems();
+            } else {
+                console.error('Product not found');
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 
 function renderRelatedItems() {
 
     let display = "";
-    const maxItems= 4;
+    const maxItems = 4;
     const itemCount = Math.min(relatedItems.length, maxItems);
 
     for (let i = 0; i < itemCount; i++) {
 
         display += `
               <div class="col my-5">
-                    <a href="/product">
+                    <a href="/product?productId=${relatedItems[i].productId}">
                     <div class="card" style="width: 18rem;">
                         <img src=${relatedItems[i].productImages} class="card-img-top py-3" alt="...">
                         <div class="card-body">
@@ -54,7 +73,7 @@ function renderRelatedItems() {
                     </div>
                     </a>
               </div>
-            `
+            `;
     }
 
     document.querySelector("#relrow").innerHTML = display;
