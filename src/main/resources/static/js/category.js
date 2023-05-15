c3316eb382d459ffca0d94ad39ce96a17b17ea18
mapHive <assembly@kiranbrar.dev>
2023-05-15 16:12:28 +0800

    Changes

---
+++ src/main/resources/static/js/category.js
@@ -0,0 +1,15 @@
+function setupCategoryListeners() {
+    // Get all anchor elements
+    let anchors = document.querySelectorAll('.list-group-item');
+
+    // Add a click event listener to each anchor
+    anchors.forEach(function(anchor) {
+        anchor.addEventListener('click', function(event) {
+            // Get the category from the id attribute
+            let category = event.currentTarget.id;
+
+            // Modify the href attribute to include the category as a query parameter
+            event.currentTarget.href = `shop.html?category=${category}`;
+        });
+    });
+}
