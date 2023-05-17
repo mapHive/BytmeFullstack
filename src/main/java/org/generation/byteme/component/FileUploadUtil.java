10070a02d636e0157cd3104b989f9b48dc3b4932
mapHive <assembly@kiranbrar.dev>
2023-05-17 12:41:08 +0800

    Changes

--- src/main/java/org/generation/byteme/component/FileUploadUtil.java
+++ src/main/java/org/generation/byteme/component/FileUploadUtil.java
@@ -1,5 +1,6 @@
 package org.generation.byteme.component;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.azure.storage.blob.*;
@@ -10,31 +11,42 @@ import java.nio.file.*;
 
 public class FileUploadUtil {
 
-    public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException
+    public static String saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException
     {
-//        Path uploadPath = Paths.get(uploadDir);
-//        try (InputStream inputStream = multipartFile.getInputStream()) {
-//
-//            Path filePath = uploadPath.resolve(fileName);
-//            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-//        } catch (IOException ioe) {
-//            throw new IOException("Could not save image file: " + fileName, ioe);
-//        }
-
-        /* This is the setup using Azure storage */
-        String connectStr2 = "DefaultEndpointsProtocol=https;AccountName=bytemeproductimages;AccountKey=RCEQbYIsfgPJkuyyUJlalGHXWVqtHLdf1mFpQ3Uip8HQtjj+VzivOPJ+/uqx3VWzuCNR3uQNtSpf+AStljB0ag==;EndpointSuffix=core.windows.net";
+        String connectStr2 = "...";
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr2).buildClient();
         String containerName = "productimages";
-
-
         BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
 
-
+        fileName = StringUtils.cleanPath(fileName);
         BlobClient blobClient = containerClient.getBlobClient(fileName);
 
-
         InputStream inputStream = multipartFile.getInputStream();
-        blobClient.upload(inputStream);
+        blobClient.upload(inputStream, multipartFile.getSize(), true);
 
+        return blobClient.getBlobUrl();
     }
 }
+
+//public class FileUploadUtil {
+//
+//    public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException
+//    {
+//
+//        /* This is the setup using Azure storage */
+//        String connectStr2 = "DefaultEndpointsProtocol=https;AccountName=bytemeproductimages;AccountKey=RCEQbYIsfgPJkuyyUJlalGHXWVqtHLdf1mFpQ3Uip8HQtjj+VzivOPJ+/uqx3VWzuCNR3uQNtSpf+AStljB0ag==;EndpointSuffix=core.windows.net";
+//        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr2).buildClient();
+//        String containerName = "productimages";
+//
+//
+//        BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+//
+//
+//        BlobClient blobClient = containerClient.getBlobClient(fileName);
+//
+//
+//        InputStream inputStream = multipartFile.getInputStream();
+//        blobClient.upload(inputStream);
+//
+//    }
+//}
--- src/main/java/org/generation/byteme/controller/ProductController.java
+++ src/main/java/org/generation/byteme/controller/ProductController.java
@@ -49,18 +49,26 @@ public class ProductController {
         BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
 
 
-        //productimagesping
-        BlobClient blobClient = containerClient.getBlobClient(productService.all().get(0).getProductImages());
-        System.out.println(blobClient);
-
-
-        //Loop through the ArrayList of itemService.all() and append the Blob url to the imageUrl
-        for (Product image: productService.all())
+        //Loop through the ArrayList of productService.all() and append the Blob url to the imageUrls
+        for (Product product: productService.all())
         {
-            //path: productimagespring/prodimage/t-shirt1.jpg
-            String setURL = blobClient.getAccountUrl() + "/" + containerName + "/" + image.getProductImages();
-            image.setProductImages(setURL);
-            System.out.println(setURL);
+            // For productImage1
+            BlobClient blobClient1 = containerClient.getBlobClient(product.getProductImage1());
+            String setURL1 = blobClient1.getAccountUrl() + "/" + containerName + "/" + product.getProductImage1();
+            product.setProductImage1(setURL1);
+            System.out.println(setURL1);
+
+            // For productImage2
+            BlobClient blobClient2 = containerClient.getBlobClient(product.getProductImage2());
+            String setURL2 = blobClient2.getAccountUrl() + "/" + containerName + "/" + product.getProductImage2();
+            product.setProductImage2(setURL2);
+            System.out.println(setURL2);
+
+            // For productImage3
+            BlobClient blobClient3 = containerClient.getBlobClient(product.getProductImage3());
+            String setURL3 = blobClient3.getAccountUrl() + "/" + containerName + "/" + product.getProductImage3();
+            product.setProductImage3(setURL3);
+            System.out.println(setURL3);
         }
 
         //return in the Controller represents a response to the Client
@@ -84,7 +92,25 @@ public class ProductController {
 
     // Set up post mapping and request
 
-    @CrossOrigin
+//    @CrossOrigin
+//    @PostMapping("/add")
+//    public void save(@RequestParam(name="productName", required = true) String productName,
+//                     @RequestParam(name="productPrice", required = true) double productPrice,
+//                     @RequestParam(name="productQuantity", required = true) Integer productQuantity,
+//                     @RequestParam(name="productCategory", required = true) String productCategory,
+//                     @RequestParam(name="productDescription", required = true) String productDescription,
+//                     @RequestParam(name="productOptions", required = false) String productOptions,
+//                     @RequestParam(name="productImages", required = true) String productImages,
+//                     @RequestParam("imagefile") MultipartFile multipartFile) throws IOException
+//    {
+//        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+//
+//        FileUploadUtil.saveFile(imageFolder, fileName, multipartFile);
+//
+//        ProductDTO productDto = new ProductDTO(productName, productPrice, productQuantity, productCategory, productDescription, productOptions, productImages);
+//        productService.save(new Product(productDto));
+//    }
+
     @PostMapping("/add")
     public void save(@RequestParam(name="productName", required = true) String productName,
                      @RequestParam(name="productPrice", required = true) double productPrice,
@@ -92,14 +118,21 @@ public class ProductController {
                      @RequestParam(name="productCategory", required = true) String productCategory,
                      @RequestParam(name="productDescription", required = true) String productDescription,
                      @RequestParam(name="productOptions", required = false) String productOptions,
-                     @RequestParam(name="productImages", required = true) String productImages,
-                     @RequestParam("imagefile") MultipartFile multipartFile) throws IOException
+                     @RequestParam(name="productImage1", required = true) String productImage1,
+                     @RequestParam("imagefile1") MultipartFile multipartFile1,
+                     @RequestParam(name="productImage2", required = true) String productImage2,
+                     @RequestParam("imagefile2") MultipartFile multipartFile2,
+                     @RequestParam(name="productImage3", required = true) String productImage3,
+                     @RequestParam("imagefile3") MultipartFile multipartFile3) throws IOException
     {
-        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        FileUploadUtil.saveFile(imageFolder, productImage1, multipartFile1);
+        FileUploadUtil.saveFile(imageFolder, productImage2, multipartFile2);
+        FileUploadUtil.saveFile(imageFolder, productImage3, multipartFile3);
 
-        FileUploadUtil.saveFile(imageFolder, fileName, multipartFile);
-
-        ProductDTO productDto = new ProductDTO(productName, productPrice, productQuantity, productCategory, productDescription, productOptions, productImages);
+        ProductDTO productDto = new ProductDTO(productName, productPrice, productQuantity, productCategory, productDescription, productOptions, productImage1, productImage2, productImage3);
         productService.save(new Product(productDto));
     }
+
+
+
 }
